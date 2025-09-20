0257861325f11559bea701c74667d57fade2429a
Adam Pozorski <pozorskia@msoe.edu>
2025-09-20 02:17:00 -0500

    Initial flask-app

---
+++ gunicorn_config.py
@@ -0,0 +1,10 @@
+
+# gunicorn_config.py
+
+# Bind to localhost on port 8000 (you can change to 0.0.0.0 to expose externally)
+bind = "0.0.0.0:8000"
+
+# Number of worker processes (adjust to your CPU cores)
+workers = 2
+
+
